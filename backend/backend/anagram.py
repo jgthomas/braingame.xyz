1672ddf7c74d054e2da46e0283f0bcf7fce2227b
James Thomas <codeactor@protonmail.com>
2020-06-13 14:40:58 +0100

    Pass a shuffled anagram to the frontend

--- backend/backend/anagram.py
+++ backend/backend/anagram.py
@@ -4,9 +4,17 @@ from backend.load_words import load_common_words, load_common_words_by_length
 
 def anagram_word():
     words = load_common_words()
-    return random.choice(words)
+    word = random.choice(words)
+    return shuffle_anagram(word)
 
 
 def anagram_word_by_length(word_length):
     word_length_dictionary = load_common_words_by_length()
-    return random.choice(word_length_dictionary[word_length])
+    word = random.choice(word_length_dictionary[word_length])
+    return shuffle_anagram(word)
+
+
+def shuffle_anagram(word):
+    word_letters = list(word)
+    random.shuffle(word_letters)
+    return "".join(word_letters)
