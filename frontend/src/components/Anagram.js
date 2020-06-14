00b6645cb2335570b50c4b43844f62b1da8a702d
James Thomas <codeactor@protonmail.com>
2020-06-14 12:37:33 +0100

    Add length selector component

--- frontend/src/components/Anagram.js
+++ frontend/src/components/Anagram.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import GuessForm from "./GuessForm";
 import Score from "./Score";
+import LengthSelector from "./LengthSelector";
 
 const Anagram = () => {
-  const [length] = useState(6);
+  const [length, setLength] = useState(6);
   const [anagram, setAnagram] = useState("");
   const [solutions, setSolutions] = useState([]);
   const [score, setScore] = useState(0);
@@ -24,11 +25,17 @@ const Anagram = () => {
     setScore(score + 1);
   };
 
+  const changeLength = (newLength) => {
+    setLength(newLength);
+    setScore(0);
+  };
+
   return (
     <div className="anagramDisplay">
       <p>Anagram word: {anagram}</p>
       <GuessForm solutions={solutions} incrementScore={incrementScore} />
       <Score score={score} />
+      <LengthSelector maxLength={9} minLength={4} changeLength={changeLength} />
     </div>
   );
 };
