ca519208032a183a093d9c1c8e2ea32485086b86
James Thomas <codeactor@protonmail.com>
2020-06-22 09:36:43 +0100

    Use number input element to select anagram length

--- frontend/src/components/LengthSelector.js
+++ frontend/src/components/LengthSelector.js
@@ -2,24 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const LengthSelector = ({ minLength, maxLength, changeLength }) => {
-  const lengths = Array.from(Array(maxLength + 1).keys()).filter(
-    (n) => n > minLength
+  return (
+    <div>
+      <form>
+        <label>Anagram length </label>
+        <input
+          data-testid="widthInput"
+          onChange={(e) => changeLength(e.target.value)}
+          type="number"
+          step="1"
+          defaultValue={maxLength - minLength}
+          min={minLength}
+          max={maxLength}
+          name="petalWidth"
+          required
+        />
+      </form>
+    </div>
   );
-
-  const optionsList = lengths.map((n) => {
-    return (
-      <option key={n} value={n}>
-        {n} Letters
-      </option>
-    );
-  });
-
-  const setNewLength = (event) => {
-    const newLength = event.target.value;
-    changeLength(newLength);
-  };
-
-  return <select onChange={setNewLength}>{optionsList}</select>;
 };
 
 LengthSelector.propTypes = {
