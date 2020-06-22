c577b93878077b27731d808f30cfaac18c8c9073
James Thomas <codeactor@protonmail.com>
2020-06-22 16:47:43 +0100

    Increase guess box size

--- frontend/src/components/GuessForm.js
+++ frontend/src/components/GuessForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./GuessForm.css";
 
 const GuessForm = ({ solutions, incrementScore, disabled }) => {
   const checkAnswer = (event) => {
