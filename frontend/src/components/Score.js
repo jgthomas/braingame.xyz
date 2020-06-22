6053898d72a05842eaa71f5be68c035476b5953f
James Thomas <codeactor@protonmail.com>
2020-06-22 09:21:58 +0100

    Add prop types

--- frontend/src/components/Score.js
+++ frontend/src/components/Score.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Score = ({ score }) => {
   return <p>Score: {score}</p>;
+};
+
+Score.propTypes = {
+  score: PropTypes.number,
 };
 
 export default Score;
