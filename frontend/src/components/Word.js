3bb9680e492d3c52b9aad0005f35ca228a128613
James Thomas <codeactor@protonmail.com>
2020-06-28 11:12:52 +0100

    Explicit control of anagram margins

--- frontend/src/components/Word.js
+++ frontend/src/components/Word.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import "./Word.css";
 
 const Word = ({ word }) => {
-  return <p class="word">{word}</p>;
+  return <div class="word">{word}</div>;
 };
 
 Word.propTypes = {
