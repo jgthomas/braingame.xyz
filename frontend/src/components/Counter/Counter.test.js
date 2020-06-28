fcc4df0c1fc79420ab9f7db2b34859e4ebbeee68
James Thomas <codeactor@protonmail.com>
2020-06-28 16:50:59 +0100

    Remove unneeded import

--- frontend/src/components/Counter/Counter.test.js
+++ frontend/src/components/Counter/Counter.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup, getByTestId } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import Counter from "./Counter";
 import Status from "../../signals/Status";
 
