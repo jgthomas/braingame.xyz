a3d895da3fdad888c18e734d855b4671fb2a5b85
James Thomas <codeactor@protonmail.com>
2020-09-25 14:07:10 +0100

    First use of styled components

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,4 +1,31 @@
-import Game from "../Game/Game";
+import React from "react";
+import styled from "styled-components";
 import Navigation from "../Navigation/Navigation";
 
-export default Game(Navigation, "BrainGame XYZ");
+const Header = styled.header`
+  background-color: cornflowerblue;
+  margin-top: 0;
+`;
+
+const Title = styled.h1`
+  color: cornsilk;
+  font-size: 3.5em;
+  margin-top: 0;
+
+  @media only screen and (max-width: 730px) {
+    font-size: 3em;
+  }
+`;
+
+const Home = () => {
+  return (
+    <div>
+      <Header>
+        <Title>BrainGame XYZ</Title>
+      </Header>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Home;
