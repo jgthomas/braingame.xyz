3490d6fbe61cf8c447019402d0d0f89f74737456
James Thomas <codeactor@protonmail.com>
2020-09-26 10:12:40 +0100

    Refactor main app component to use styled components

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 import Home from "../Home/Home";
 import Anagram from "../Anagram/Anagram";
-import "./App.css";
+
+const AppBody = styled.div`
+  text-align: center;
+`;
 
 const App = () => {
   return (
-    <div className="App">
+    <AppBody>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/anagram" component={Anagram} />
       </Switch>
-    </div>
+    </AppBody>
   );
 };
 
