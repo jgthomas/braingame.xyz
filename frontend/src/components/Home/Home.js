6cf048b94dd73d0ebb70957fd1c8dd26ca9ea9cc
James Thomas <codeactor@protonmail.com>
2020-09-26 10:38:13 +0100

    Move all home page styling into component

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 import Navigation from "../Navigation/Navigation";
-import { HomePageBody, SiteHeader, SiteTitle } from "./HomeStyles";
+import HeaderStyled from "../../styles/HeaderStyled";
+import TitleStyled from "../../styles/TitleStyled";
+import PageBodyStyled from "../../styles/PageBodyStyled";
+
+const homePrimary = "cornsilk";
+const homeBackground = "cornflowerblue";
+
+const SiteHeader = styled(HeaderStyled)`
+  background-color: ${homeBackground}};
+`;
+
+const SiteTitle = styled(TitleStyled)`
+  color: ${homePrimary};
+`;
+
+const HomePageBody = styled(PageBodyStyled)`
+  background-color: ${homePrimary};
+`;
 
 const Home = () => {
   return (
