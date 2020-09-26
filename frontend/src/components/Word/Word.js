a759033601c21331e4087d9f26e65ec7a67c085e
James Thomas <codeactor@protonmail.com>
2020-09-26 08:53:39 +0100

    Word moved to styled components

--- frontend/src/components/Word/Word.js
+++ frontend/src/components/Word/Word.js
@@ -1,13 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Word.css";
+import styled from "styled-components";
+
+const WordDisplay = styled.div`
+  letter-spacing: 0.0625em;
+  font-size: 2.5em;
+  color: #d8482d;
+  margin-top: 1em;
+  margin-bottom: 1em;
+
+  @media only screen and (min-width: 750px) {
+    font-size: 4em;
+  }
+
+  @media only screen and (min-width: 900px) {
+    font-size: 4.5em;
+  }
+`;
 
 const Word = ({ word }) => {
-  return (
-    <div className="word" data-testid="word-display">
-      {word}
-    </div>
-  );
+  return <WordDisplay data-testid="word-display">{word}</WordDisplay>;
 };
 
 Word.propTypes = {
--- frontend/src/components/Word/Word.test.js
+++ frontend/src/components/Word/Word.test.js
@@ -7,10 +7,8 @@ afterEach(cleanup);
 describe("Word Component", () => {
   it("should display the passed word", () => {
     const expectedWord = "doglets";
-    const expectedClass = "word";
-    const { getByTestId, getByText } = render(<Word word={expectedWord} />);
+    const { getByTestId } = render(<Word word={expectedWord} />);
 
     expect(getByTestId("word-display")).toHaveTextContent(expectedWord);
-    expect(getByText(expectedWord)).toHaveClass(expectedClass);
   });
 });
