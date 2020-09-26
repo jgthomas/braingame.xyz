61673db95506b6dc6f3cf376bb5c9e74a5e33c4e
James Thomas <codeactor@protonmail.com>
2020-09-26 15:35:35 +0100

    Remove redundant navigation component

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import Navigation from "../Navigation/Navigation";
 import HeaderStyled from "../../styles/HeaderStyled";
 import TitleStyled from "../../styles/TitleStyled";
 import PageBodyStyled from "../../styles/PageBodyStyled";
 import PageDesign from "../../styles/PageDesign";
+import * as Links from "../GameLink/Links";
 
 const Design = PageDesign.home;
+
+const HomePageBody = styled(PageBodyStyled)`
+  background-color: ${Design.primary};
+`;
 
 const SiteHeader = styled(HeaderStyled)`
   background-color: ${Design.header}};
@@ -23,8 +27,38 @@ const SiteTitle = styled(TitleStyled)`
   color: ${Design.title};
 `;
 
-const HomePageBody = styled(PageBodyStyled)`
-  background-color: ${Design.primary};
+const LinkGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 5% 2%;
+  align-items: stretch;
+  width: 50%;
+  max-width: 1000px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 2em;
+
+  @media only screen and (max-width: 900px) {
+    width: 75%;
+  }
+`;
+
+const homePageLinkStyle = `font-size: 250%;`;
+
+const HomePageAnagram = styled(Links.AnagramLink)`
+  ${homePageLinkStyle}
+`;
+
+const HomePageLadder = styled(Links.LadderLink)`
+  ${homePageLinkStyle}
+`;
+
+const HomePageGrid = styled(Links.GridLink)`
+  ${homePageLinkStyle}
+`;
+
+const HomePageRack = styled(Links.RackLink)`
+  ${homePageLinkStyle}
 `;
 
 const Home = () => {
@@ -33,7 +67,12 @@ const Home = () => {
       <SiteHeader>
         <SiteTitle>BrainGame</SiteTitle>
       </SiteHeader>
-      <Navigation />
+      <LinkGrid>
+        <HomePageAnagram routeName="Anagram"></HomePageAnagram>
+        <HomePageLadder routeName="Ladder"></HomePageLadder>
+        <HomePageGrid routeName="Grid"></HomePageGrid>
+        <HomePageRack routeName="Rack"></HomePageRack>
+      </LinkGrid>
     </HomePageBody>
   );
 };
