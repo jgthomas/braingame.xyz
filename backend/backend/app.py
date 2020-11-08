c1d008c2d5e6d2d7b6bf19c7e875933891cc27c3
James Thomas <codeactor@protonmail.com>
2020-11-08 11:01:08 +0000

    Prod docker file now runs gunicorn

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -3,12 +3,10 @@ from fastapi import FastAPI
 from backend.anagram import anagram_puzzle
 
 
-def create_app():
-    app = FastAPI()
+app = FastAPI()
 
-    @app.get("/anagram")
-    def anagram(length: int):
-        anagram, solutions = anagram_puzzle(length)
-        return {"word": anagram, "solutions": solutions}
 
-    return app
+@app.get("/anagram")
+def anagram(length: int):
+    anagram, solutions = anagram_puzzle(length)
+    return {"word": anagram, "solutions": solutions}
--- backend/backend/app_dev.py
+++ backend/backend/app_dev.py
@@ -1,8 +1,6 @@
 import uvicorn
-from app import create_app
 
 
 if __name__ == "__main__":
-    app = create_app()
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run("app:app", host="0.0.0.0", port=5000)
     # app.run(host="0.0.0.0", port=5000, debug=True)
