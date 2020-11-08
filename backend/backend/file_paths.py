8fb6a5ca51cd2d6925f049b61d5a64217d61171b
James Thomas <codeactor@protonmail.com>
2020-11-08 14:40:38 +0000

    Tests for word file paths

--- backend/backend/file_paths.py
+++ backend/backend/file_paths.py
@@ -1,24 +1,27 @@
 import os
 
-file_dir = os.path.dirname(os.path.realpath("__file__"))
+CONTAINER_ROOT = "/app/backend"
 
-local_path = {
+current_root = os.path.dirname(os.path.realpath("__file__"))
+
+local = {
     "nine": "backend/static/wordlist/nine_letter_words.txt",
     "dict": "backend/static/wordlist/sowpods.txt",
     "common": "backend/static/wordlist/50k.txt",
 }
 
-continer_path = {
+container = {
     "nine": "static/wordlist/nine_letter_words.txt",
     "dict": "static/wordlist/sowpods.txt",
     "common": "static/wordlist/50k.txt",
 }
 
-if file_dir == "/app/backend":
-    files = continer_path
-else:
-    files = local_path
 
-NINE_LETTER_WORDS = os.path.join(file_dir, files["nine"])
-DICTIONARY = os.path.join(file_dir, files["dict"])
-COMMON_WORDS = os.path.join(file_dir, files["common"])
+def set_file_path(current_root, filename):
+    files = container if current_root == CONTAINER_ROOT else local
+    return os.path.join(current_root, files[filename])
+
+
+NINE_LETTER_WORDS = set_file_path(current_root, "nine")
+DICTIONARY = set_file_path(current_root, "dict")
+COMMON_WORDS = set_file_path(current_root, "common")
---
+++ backend/tests/test_file_paths.py
@@ -0,0 +1,46 @@
+import os
+import backend.file_paths
+from backend.file_paths import set_file_path
+
+
+def root_dir():
+    return os.path.dirname(os.path.realpath(backend.file_paths.__file__))
+
+
+def test_container_file_path_nine_letters():
+    expected = "/app/backend/static/wordlist/nine_letter_words.txt"
+    actual = set_file_path("/app/backend", "nine")
+    assert expected == actual
+
+
+def test_container_file_path_sowpods_dictionary():
+    expected = "/app/backend/static/wordlist/sowpods.txt"
+    actual = set_file_path("/app/backend", "dict")
+    assert expected == actual
+
+
+def test_container_file_path_common_words():
+    expected = "/app/backend/static/wordlist/50k.txt"
+    actual = set_file_path("/app/backend", "common")
+    assert expected == actual
+
+
+def test_local_file_path_nine_letters():
+    current_root = root_dir()
+    expected = f"{current_root}/backend/static/wordlist/nine_letter_words.txt"
+    actual = set_file_path(current_root, "nine")
+    assert expected == actual
+
+
+def test_local_file_path_sowpods_dictionary():
+    current_root = root_dir()
+    expected = f"{current_root}/backend/static/wordlist/sowpods.txt"
+    actual = set_file_path(current_root, "dict")
+    assert expected == actual
+
+
+def test_local_file_path_common_words():
+    current_root = root_dir()
+    expected = f"{current_root}/backend/static/wordlist/50k.txt"
+    actual = set_file_path(current_root, "common")
+    assert expected == actual
